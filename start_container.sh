7239babbb3996113c49ecf2274ebe4231fcc2b98
yuvanedvin <87174764+yuvanedvin@users.noreply.github.com>
2025-04-18 13:29:53 +0000

    last modification

--- start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull yuvan19/simple-python-flask-app:latest
-
-# Run the Docker image as a container
-# With the Port mentioned in Dockerfile along with image name
-docker run -d -p 5000:5000 yuvan19/simple-python-flask-app:latest
--- stop_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Stop the running container (if any)
-
-echo "Stopping container..."
-docker stop my_container || true
-
